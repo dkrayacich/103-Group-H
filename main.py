0f78c4c7ed873ebbc580f53a06523ff68aedf71d
dkrayacich <david.krayacich@gmail.com>
2024-03-13 15:16:54 -0400

    n/a

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ def main():
     rover = Rover() # this line starts the connection to the rover and gives access to the rover data
     angle_reader = AngleReader()
 
-    tooLeft = False
-    tooRight = False
+
+    left_side_speed = 0
+    right_side_speed = 0
     while True:
 
         if rover.laser_distances[18] > 0.5 and rover.laser_distances[19] > 0.5 and rover.laser_distances[20] > 0.5:
             while rover.laser_distances[18] > 0.5 and rover.laser_distances[19] > 0.5 and rover.laser_distances[20] > 0.5:
                 left_side_speed = 1
                 right_side_speed = -1
+                rover.send_command(left_side_speed, right_side_speed)
 
 
         if rover.laser_distances[14] < 0.5 and rover.laser_distances[15] < 0.5 and rover.laser_distances[16] < 0.5:
             while rover.laser_distances[14] < 0.5 and rover.laser_distances[15] < 0.5 and rover.laser_distances[16] < 0.5:
                 left_side_speed = -1
                 right_side_speed = 1
+                rover.send_command(left_side_speed, right_side_speed)
 
