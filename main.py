2134d8cb6822a62513f5d64f67ce16f294e489e0
dkrayacich <david.krayacich@gmail.com>
2024-03-15 19:15:30 -0400

    n/a

--- main.py
+++ main.py
@@ -23,4 +23,5 @@ def main():
                 left_side_speed = -1
                 right_side_speed = 1
                 rover.send_command(left_side_speed, right_side_speed)
+                #hello
 
